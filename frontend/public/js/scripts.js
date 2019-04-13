74f9d39d4bd65dd87cc0913827c87130d55b64a9
popov <popov@adn.agency>
2019-04-13 12:47:15 +0700

    add question pages. Some page in processing

--- frontend/public/js/scripts.js
+++ frontend/public/js/scripts.js
@@ -1,43 +1,27 @@
-$("#question-type").change(function (event) {
-    $('#question-type').parent(".form-group").nextAll().css("display","none");
-    $("#" + $(this).val()).css('display',"block");
-});
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        --timer;
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
-//choose
-$("button[name='add-ans-ch']").click(function (event) {
-    event.preventDefault();
-    var n1 = $(this).parent(".col-10").find("input").last().attr("name").substr(4,1);
-    n1 = +n1 + 1;
-    var block;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    block = $(this).parent(".col-10").find("input").last();
-    $("<input type='text' name='ans-"+ n1 + "-ch' class='form-control' placeholder='Ответ'>").insertAfter(block);
+        display.textContent = minutes + ":" + seconds;
 
-    block = $(this).parent(".col-10").parent(".row").find(".col-2 div").last();
-    $("<div class=\"radio-label-div\"><label for='ans-" + n1 +"-ch' class=\"radio-label\"><input type='radio' name='ans-right-ch' value='" + n1 +"'></label></div>").insertAfter(block);
-});
+        if (timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
 
-//multi-choose
-$("button[name='add-ans-mch']").click(function (event) {
-    event.preventDefault();
-    var n1 = $(this).parent(".col-10").find("input").last().attr("name").substr(4,1);
-    n1 = +n1 + 1;
-    var block;
-
-    block = $(this).parent(".col-10").find("input").last();
-    $("<input type='text' name='ans-"+ n1 + "-mch' class='form-control' placeholder='Ответ'>").insertAfter(block);
-
-    block = $(this).parent(".col-10").parent(".row").find(".col-2 div").last();
-    $("<div class=\"radio-label-div\"><label for='ans-" + n1 +"-mch' class=\"radio-label\"><input type='checkbox' name='ans-right-mch-" + n1 + "' value='" + n1 +"'></label></div>").insertAfter(block);
-});
-
-//docker
-$("button[name='add-ans-doc']").click(function (event) {
-    event.preventDefault();
-    var n1 = $(this).parent(".col-12").find("input").last().attr("name").substr(4,1);
-    n1 = +n1 + 1;
-    var block;
-
-    block = $(this).parent(".col-12").find("input").last();
-    $("<input type='text' name='ans-"+ n1 + "-doc' class='form-control' placeholder='Ответ'>").insertAfter(block);
-});
+window.onload = function () {
+    var display = document.querySelector('#timeLeft');
+    if(display !== null)
+    {
+        var timeArray = display.innerHTML.split(":");
+        var time = +(+timeArray[0]*60) + +timeArray[1];
+        startTimer(time, display);
+    }
+};
